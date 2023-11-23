575ff931d0722c9feb84a0bc71da71b268389c33
Rajeshsathiyan <106315909+Rajeshsathiyan@users.noreply.github.com>
2023-11-23 16:58:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("button").click(function(){
+        let div=$("div");
+        div.animate({left:"100px"},"slow");
+        div.animate({fontSize:"25px"},"slow");
+    })
+})
